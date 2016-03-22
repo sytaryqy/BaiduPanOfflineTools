794133ee51a2963286b9061395b3c0209b274bda
sytaryqy <yqy1981@163.com>
2016-03-22 16:58:01 +0800

    Load app test

---
+++ BaiduYunAppTool/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BaiduYunAppTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadApp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadApp
+            // 
+            this.btnLoadApp.Location = new System.Drawing.Point(39, 25);
+            this.btnLoadApp.Name = "btnLoadApp";
+            this.btnLoadApp.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadApp.TabIndex = 0;
+            this.btnLoadApp.Text = "加载";
+            this.btnLoadApp.UseVisualStyleBackColor = true;
+            this.btnLoadApp.Click += new System.EventHandler(this.btnLoadApp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 289);
+            this.Controls.Add(this.btnLoadApp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadApp;
+    }
+}
+
---
+++ BaiduYunAppTool/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace BaiduYunAppTool
+{
+    public partial class Form1 : Form
+    {
+        string appName = "BaiduYunGuanjia.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadApp_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //启动外部程序
+                Process proc = Process.Start(appName);
+                if (proc != null)
+                {
+                    //监视进程退出
+                    proc.EnableRaisingEvents = true;
+                    //指定退出事件方法
+                    proc.Exited += new EventHandler(proc_Exited);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        /// <summary>
+        ///启动外部程序退出事件
+        /// </summary>
+        void proc_Exited(object sender, EventArgs e)
+        {
+            MessageBox.Show(String.Format("外部程序 {0} 已经退出！", this.appName), this.Text,
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
